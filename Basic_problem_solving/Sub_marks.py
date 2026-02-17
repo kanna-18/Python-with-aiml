48b074b34e8b7ec6397b1eb6a2b3373355243e5e
kanna-18 <251801500019@cutmap.ac.in>
2026-02-17 21:10:27 +0530

    Create Sub_marks.py

---
+++ Basic_problem_solving/Sub_marks.py
@@ -0,0 +1,10 @@
+s1 = float(input("enter marks"))
+s2 =float(input("enter marks"))
+s3 =float(input("enter marks"))
+s4 =float(input("enter marks"))
+s5 =float(input("enter marks"))
+total = s1 + s2 + s3 + s4 + s5
+average = total / 5.0
+percentage = (total / 500.0) * 100
+
+print("Total marks =", total, "Average marks=",average , "Percentage =", percentage)
