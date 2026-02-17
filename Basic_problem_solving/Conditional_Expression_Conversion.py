d77de86490e0e2e06176b8dd2b701e5a1f895910
kanna-18 <251801500019@cutmap.ac.in>
2026-02-17 23:33:31 +0530

    Create Conditional_Expression_Conversion.py

---
+++ Basic_problem_solving/Conditional_Expression_Conversion.py
@@ -0,0 +1,12 @@
+#A
+a=float(input("enter a num"))
+b=float(input("enter a num"))
+x=(a+b) if ((a>10)and(b<5)) else 0
+print(x)
+
+#b
+marks = float(input("enter marks"))
+if (marks>60):
+    print("pass")
+else: 
+    print("fail")
