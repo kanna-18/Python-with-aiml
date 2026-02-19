dbf1f1c66d5d7109efc715c739a89ea2f849cedf
kanna-18 <251801500019@cutmap.ac.in>
2026-02-19 11:21:51 +0530

    Create List_sub_list_integer.py

---
+++ Basic_problem_solving/List_sub_list_integer.py
@@ -0,0 +1,5 @@
+b=[[[1,2],[3,4],[5,6]],[[1,2],[3,4],[5,6]],[[1,2],[3,4],[5,6]],[[1,2],[3,4],[5,6]]]
+print(b) #print full list
+print(b[0]) # print selested list in full list
+print(b[0][2])  # prints sub list of selected list in full list
+print(b[0][1][0])#print selected integer
